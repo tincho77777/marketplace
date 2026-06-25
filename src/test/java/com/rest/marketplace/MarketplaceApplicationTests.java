3551899b85498bc76f3eeb8012f425e61218e081
Martin Alvarez <Martin_Alvarez@itrsa.com.ar>
2026-06-25 10:58:53 -0300

    mkt-1-Se agrega perfil de test y base H2 para correr los test juntos con su correspondiente properties

--- src/test/java/com/rest/marketplace/MarketplaceApplicationTests.java
+++ src/test/java/com/rest/marketplace/MarketplaceApplicationTests.java
@@ -2,8 +2,10 @@ package com.rest.marketplace;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MarketplaceApplicationTests {
 
 	@Test
