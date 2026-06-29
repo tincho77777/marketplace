fd30a86f397ac0c08511fbe20f456008ad0fc629
Martin Alvarez <Martin_Alvarez@itrsa.com.ar>
2026-06-29 17:23:24 -0300

    mkt-1-Se agrega patron circuit braker a el alta de productos al fallar la comunicacion con rabbitmq

--- src/main/java/com/rest/marketplace/infrastructure/messaging/rabbit/ProductEventPublisher.java
+++ src/main/java/com/rest/marketplace/infrastructure/messaging/rabbit/ProductEventPublisher.java
@@ -2,6 +2,8 @@ package com.rest.marketplace.infrastructure.messaging.rabbit;
 
 import com.rest.marketplace.domain.models.events.ProductCreatedEvent;
 import com.rest.marketplace.domain.ports.out.ProductEventPort;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -21,9 +23,22 @@ public class ProductEventPublisher implements ProductEventPort {
 	@Value("${rabbitmq.routing-key}")
 	private String routingKey;
 
+	//El orden de las anotaciones importa: @CircuitBreaker va primero (más externo) y @Retry va después (más interno)
+	@Override
+	@CircuitBreaker(name = "rabbitmq", fallbackMethod = "fallbackPublishProductCreated")
+	@Retry(name = "rabbitmq")
 	public void publishProductCreated(ProductCreatedEvent event){
 		log.info("Publicando evento de producto creado: {}", event.getTitle());
 		rabbitTemplate.convertAndSend(exchange, routingKey, event);
 		log.info("Evento publicado exitosamente en exchange: {}", exchange);
+	}
+
+	public void fallbackPublishProductCreated(ProductCreatedEvent event, Exception ex) {
+		log.error("==========================================");
+		log.error("❌ Circuit Breaker ACTIVADO");
+		log.error("📦 Evento no publicado: {}", event.getTitle());
+		log.error("🔴 Causa: {}", ex.getMessage());
+		log.error("⚠️  El evento se debería guardar para reintento (Outbox Pattern)");
+		log.error("==========================================");
 	}
 }
